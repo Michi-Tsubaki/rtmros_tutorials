e4d8fd68105cf7944e2797f5e0f2d2ad76c1b4f1
Michitoshi Tsubaki <tsubaki@jsk.t.u-tokyo.ac.jp>
2025-10-24 20:47:23 +0900

    Create force sensor udev rules

--- nextage_tutorials/scripts/create_udev_rules.sh
+++ nextage_tutorials/scripts/create_udev_rules.sh
@@ -1,4 +1,3 @@
-create_udev_rules.sh 
 #!/bin/bash
 
 echo ""
@@ -6,9 +5,12 @@ echo "This scripts copies udev rules for dual hands to /etc/udev/rules.d"
 echo ""
 
 sudo cp `rospack find nextage_tutorials`/udev/99-elp-camera.rules /etc/udev/rules.d
-
+sudo cp `rospack find nextage_tutorials`/udev/99-rftsensor.rules /etc/udev/rules.d
 echo ""
 echo "Restarting udev"
 echo ""
 sudo service udev reload
 sudo service udev restart
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
