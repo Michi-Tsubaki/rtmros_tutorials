ff161200e2ecb7f757c8057c1355cfacb41f9bed
Michitoshi Tsubaki <tsubaki@jsk.t.u-tokyo.ac.jp>
2025-12-04 18:13:09 +0900

    Use leptrino from slave

--- nextage_tutorials/scripts/create_udev_rules.sh
+++ nextage_tutorials/scripts/create_udev_rules.sh
@@ -5,7 +5,7 @@ echo "This scripts copies udev rules for dual hands to /etc/udev/rules.d"
 echo ""
 
 sudo cp `rospack find nextage_tutorials`/udev/99-elp-camera.rules /etc/udev/rules.d
-sudo cp `rospack find nextage_tutorials`/udev/99-rftsensor.rules /etc/udev/rules.d
+sudo cp `rospack find nextage_tutorials`/udev/91-force_sensors.rules /etc/udev/rules.d
 echo ""
 echo "Restarting udev"
 echo ""
