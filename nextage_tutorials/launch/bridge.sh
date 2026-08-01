1ba48e2316d7d25596a2d7684a12e1e73d270a6c
Michitoshi Tsubaki <tsubaki@jsk.t.u-tokyo.ac.jp>
2026-08-01 23:10:31 +0900

    Refactor launch

--- nextage_tutorials/launch/bridge.sh
+++ nextage_tutorials/launch/bridge.sh
@@ -4,4 +4,4 @@ source ~/catkin_ws/devel/setup.bash
 rossetip
 rossetmaster 133.11.216.115
 source ~/colcon_ws/install/setup.bash
-ros2 launch ros1bridge.launch.xml
+ros2 launch raw/ros1bridge.launch.xml
